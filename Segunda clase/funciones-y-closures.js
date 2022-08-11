5421e586df4200f685c0b448a1dff046d3851255
cyphersprojekt <munoz.ignacioagustin@gmail.com>
2022-08-11 20:42:05 -0300

    clases, la cosa sana

--- Segunda clase/funciones-y-closures.js
+++ Segunda clase/funciones-y-closures.js
@@ -11,7 +11,7 @@ function mostrarListado(listado) {
 mostrarListado(['Juan', 'Pedro', 'Maria']);
 mostrarListado([]);
 
-console.log('----------------------------------------------------');
+console.log('\r\n----------------------------------------------------\r\n');
 
 function crearMultiplicador(factor) {
     return function (numero) {
@@ -23,4 +23,21 @@ const triplicar = crearMultiplicador(3);
 console.log(duplicar(10));
 console.log(triplicar(10));
 
-console.log('----------------------------------------------------');
+console.log('\r\n----------------------------------------------------\r\n');
+
+class Persona {
+    constructor(nombre, apellido, edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+    saludar() {
+        console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y tengo ${this.edad} años`);
+    }
+}
+const juancito = new Persona('Juan', '', 30);
+console.log(juancito);
+juancito.saludar();
+
+console.log('\r\n----------------------------------------------------\r\n');
+
