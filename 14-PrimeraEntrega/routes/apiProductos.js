835481e7f6eed348a3bdd8c9d2cc25b8dd70de3c
cyphersprojekt <munoz.ignacioagustin@gmail.com>
2022-10-07 20:28:33 -0300

    funcion isadmin

--- 14-PrimeraEntrega/routes/apiProductos.js
+++ 14-PrimeraEntrega/routes/apiProductos.js
@@ -3,6 +3,27 @@ const router = express.Router();
 const Contenedor = require('../helpers/contenedor.js');
 const apiContenedor = new Contenedor('14-PrimeraEntrega/data/productos.txt');
 
+const isAdmin = ((req, res, next) => {
+    const isAdmin = req.headers["isadmin"]
+    let needsAuth = false
+    if (req.method == "POST" || req.method == "PUT" || req.method == "DELETE"){
+        needsAuth = true
+    }
+
+    if (needsAuth && isAdmin == "true"){
+        next()
+    }
+    else if(!needsAuth){
+        next()
+    }
+    if(needsAuth && (isAdmin == null || isAdmin != "true")){
+        res.send({
+            error: -1,
+            descripcion: `ruta ${req.method} ${req.originalUrl} no autorizada`
+        })
+    }
+})
+
 router.get('/', (req, res) => {
     const data = apiContenedor.getAllData();
     res.send(data)
@@ -16,20 +37,20 @@ router.get('/:id', (req, res) => {
     if (!prod) {res.send(error) } else { res.send(prod); } }
 );
 
-router.post('/', (req, res) => {
+router.post('/', isAdmin, (req, res) => {
     const itemToAdd = req.body;
     res.send(apiContenedor.save(itemToAdd));
     }
 );
 
-router.put('/:id', (req, res) => {
+router.put('/:id', isAdmin, (req, res) => {
     const id = parseInt(req.params.id);
     const itemToUpdate = req.body;
     res.send(apiContenedor.update(id, itemToUpdate));
     }
 );
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', isAdmin, (req, res) => {
     const id = parseInt(req.params.id);
     res.send(apiContenedor.deleteById(id))
     }
