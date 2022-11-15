4697eb974cd3732abeaac668d7a0742f4caf3eb7
cyphersprojekt <munoz.ignacioagustin@gmail.com>
2022-11-15 19:19:22 -0300

    create blank mongooseCreds

---
+++ 24-Login/helpers/mongooseCreds.js
@@ -0,0 +1,11 @@
+let mongoCreds = {
+    mongoUrl: "",
+    autoRemove: 'native',
+    ttl: 600,
+    mongoOptions: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
+
+module.exports = mongoCreds
